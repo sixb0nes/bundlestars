f970c3aa577c36b3d458dc9af1062e71472c57eb
sixb0nes <ehiggins@live.ca>
2017-08-07 19:26:58 -0400

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,7 @@ results = []
 1.times do |i|
   doc = Nokogiri::HTML(open('https://www.bundlestars.com/api/products?page='+i.to_s+'&types=game,dlc&sale=true'))
   doc.css('.bs-card-body').each do |game|
-logger.error "Something blew up!"
+puts game
   end
 end
 
