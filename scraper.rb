aa1cb64518345f2b542e89312aa42d34a971cd58
sixb0nes <ehiggins@live.ca>
2017-08-07 19:21:42 -0400

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,23 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'open-uri'
+require 'nokogiri'
+require 'scraperwiki'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+results = []
+
+1.times do |i|
+  doc = Nokogiri::HTML(open('https://www.bundlestars.com/api/products?page='+i+'&types=game,dlc&sale=true'.to_s))
+  doc.css('.search_result_row').each do |game|
+    price_el = game.at_css('.search_price')
+    price_el.children.each { |c| c.remove if c.name == 'span' }
+    price = price_el.text.strip.split(' ')[0]
+    id = game["data-ds-appid"].to_i
+    puts id, price
+    results.push({id: id, price: price})
+  end
+end
+
+results.each do |result|
+  ScraperWiki.save_sqlite([:id], result)
+end
